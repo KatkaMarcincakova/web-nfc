543f3f105017e07d0b9e1cbc59a2d9663afd2192
Katka Marcincakova <katulik137@gmail.com>
2023-05-14 00:49:03 +0200

    Update 1-read-write.js

--- 1-read-write.js
+++ 1-read-write.js
@@ -1,39 +1,23 @@
 var nfc = {
   // (A) INIT
-  hTxt : null, // html data to write
-  hWrite : null, // html write button
+  hTxt : null,
   hRead : null, // html read button
   hMsg : null, // html "console messages"
   init : () => {
     // (A1) GET HTML ELEMENTS
-    nfc.hTxt = document.getElementById("demoT"),
-    nfc.hWrite = document.getElementById("demoW"),
     nfc.hRead = document.getElementById("demoR"),
     nfc.hMsg = document.getElementById("demoMSG");
 
     // (A2) FEATURE CHECK + GET PERMISSION
     if ("NDEFReader" in window) {
       nfc.logger("Ready");
-      nfc.hWrite.disabled = false;
       nfc.hRead.disabled = false;
-      nfc.hReadOnly.disabled = false;
     } else { nfc.logger("Web NFC is not supported on this browser."); }
   },
 
   // (B) HELPER - DISPLAY LOG MESSAGE
   logger : msg => {
-    let row = document.createElement("div");
-    row.innerHTML = msg;
-    nfc.hMsg.appendChild(row);
-  },
-
-  // (C) WRITE NFC TAG
-  write : () => {
-    nfc.logger("Approach NFC Tag");
-    const ndef = new NDEFReader();
-    ndef.write(nfc.hTxt.value)
-    .then(() => nfc.logger("Write OK"))
-    .catch(err => nfc.logger("ERROR - " + err.message));
+    nfc.hMsg.innerHTML = msg;
   },
 
   // (D) READ NFC TAG
@@ -46,8 +30,6 @@ var nfc = {
       ndef.onreading = evt => {
         const decoder = new TextDecoder();
         for (let record of evt.message.records) {
-          nfc.logger("Record type: " + record.recordType);
-          nfc.logger("Record encoding: " + record.encoding);
           nfc.logger("Record data: " + decoder.decode(record.data));
         }
       };
