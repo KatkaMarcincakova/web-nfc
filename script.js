2529e91b592ec4f935fc7874b4965ce88262bf7c
Katka Marcincakova <katulik137@gmail.com>
2023-05-14 10:24:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,23 @@
-if ('NDEFReader' in window) {
-
-    navigator.permissions.query({ name: "nfc" }).then((nfcStatus) => {
-
-        if (nfcStatus.state === "granted") {
-            startScanning();
-        } else {
-
-          document.querySelector("#scanButton").onclick = event => {
-            startScanning();
-          };
-
-        }
-
-    });
-
-    function startScanning(){
+scanButton.addEventListener("click", async () => {
+  console.log("User clicked scan button");
+  if ('NDEFReader' in window) {
+      try {
         const ndef = new NDEFReader();
-        var hMsg = document.getElementById("demoMSG");
-        ndef.scan().then(() => {
+        await ndef.scan();
+        console.log("> Scan started");
 
-          ndef.onreadingerror = () => {
-             let row = document.createElement("div");
-              row.innerHTML = "reading error";
-              hMsg.appendChild(row);
-          };
-
-          ndef.onreading = event => {
-              if (event.recordType === "text") {
-                  const textDecoder = new TextDecoder(event.encoding);
-                  console.log(`Text Record: ${textDecoder.decode(event.data)} ${record.lang}`);
-                  let row = document.createElement("div");
-                  row.innerHTML = "text record: " + textDecoder.decode(event.data) + " " + record.lang;
-                  hMsg.appendChild(row);
-              }
-          }
-
-        }).catch(error => {
-          let row = document.createElement("div");
-              row.innerHTML = "something wrong with hw";
-              hMsg.appendChild(row);
+        ndef.addEventListener("readingerror", () => {
+          console.log("Argh! Cannot read data from the NFC tag. Try another one?");
         });
-    }
 
-}
+        ndef.addEventListener("reading", ({ message, serialNumber }) => {
+          console.log(`> Serial Number: ${serialNumber}`);
+          console.log(`> Records: (${message.records.length})`);
+        });
+      } catch (error) {
+        console.log("Argh! " + error);
+      }
+  } else {
+    console.log("no nfc");
+  }
+});
