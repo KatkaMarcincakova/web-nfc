a05b53ab55c3d1ae2a9d528821d4291f03fbcefd
Katka Marcincakova <katulik137@gmail.com>
2023-05-14 10:15:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-// Check if the browser supports the Web NFC API
-if ("NDEFReader" in window) {
-  // Define the function to read the NFC tag
-  async function readNfc() {
-    try {
-      // Request permission to access the NFC reader
-      const reader = new NDEFReader();
-      await reader.scan();
+if ('NDEFReader' in window) {
 
-      // Wait for the NFC tag to be read
-      const { records } = await reader.read();
+    navigator.permissions.query({ name: "nfc" }).then((nfcStatus) => {
 
-      // Display the tag data on the webpage
-      const tagDataDiv = document.getElementById("tagData");
-      tagDataDiv.innerHTML = records[0].data;
-    } catch (error) {
-      const tagDataDiv = document.getElementById("tagData");
-      tagDataDiv.innerHTML = "Error reading NFC tag:" + error;
+        if (nfcStatus.state === "granted") {
+            startScanning();
+        } else {
+
+          document.querySelector("#scanButton").onclick = event => {
+            startScanning();
+          };
+
+        }
+
+    });
+
+    function startScanning(){
+        const ndef = new NDEFReader();
+        var hMsg = document.getElementById("demoMSG");
+        ndef.scan().then(() => {
+
+          ndef.onreadingerror = () => {
+             let row = document.createElement("div");
+              row.innerHTML = "reading error";
+              hMsg.appendChild(row);
+          };
+
+          ndef.onreading = event => {
+              if (event.recordType === "text") {
+                  const textDecoder = new TextDecoder(event.encoding);
+                  console.log(`Text Record: ${textDecoder.decode(event.data)} ${record.lang}`);
+                  let row = document.createElement("div");
+                  row.innerHTML = "text record: " + textDecoder.decode(event.data) + " " + record.lang;
+                  hMsg.appendChild(row);
+              }
+          }
+
+        }).catch(error => {
+          let row = document.createElement("div");
+              row.innerHTML = "something wrong with hw";
+              hMsg.appendChild(row);
+        });
     }
-  }
-} else {
-      const tagDataDiv = document.getElementById("tagData");
-      tagDataDiv.innerHTML = "NFC not supported";
+
 }
